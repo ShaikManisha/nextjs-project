1614a4af34e3d2d42bb9df964816d6df62ab019e
Shaik Manisha <manishashaik0606@gmail.com>
2024-03-19 15:36:23 +0530

    Learn routes and dynmaic routes

---
+++ src/app/blog/first/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function FirstBlog() {
+  return <div>first Blog page</div>;
+}
---
+++ src/app/products/[productId]/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function ProductDetails({
+  params,
+}: {
+  params: { productId: string };
+}) {
+  return (
+    <>
+      <h1>Product Details {params.productId}</h1>
+    </>
+  );
+}
