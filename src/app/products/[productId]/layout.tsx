821c35c018e86cd0d814a689f0839b816463e8a5
Shaik Manisha <manishashaik0606@gmail.com>
2024-03-26 14:27:31 +0530

    Learned Layouts and nested layouts

---
+++ src/app/products/[productId]/layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ProductDeatilsLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      {children}
+      <h2>ProductDeatilsLayout</h2>
+    </>
+  );
+};
+
+export default ProductDeatilsLayout;
