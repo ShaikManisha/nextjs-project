8ce9af904e68c2cffec35302e1fa0cfcf91bdca2
Shaik Manisha <manishashaik0606@gmail.com>
2024-03-27 15:03:50 +0530

    Learned Navigating dynamic routes and navigating programatically

---
+++ src/app/order-product/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+import React from "react";
+import { useRouter } from "next/navigation";
+
+const OrderProduct = () => {
+  const router = useRouter();
+  const handleClick = () => {
+    console.log("placeing order");
+    router.push("/");
+  };
+  return (
+    <>
+      <h1>OrderProduct</h1>
+      <button onClick={handleClick}>Place Order</button>
+    </>
+  );
+};
+
+export default OrderProduct;
