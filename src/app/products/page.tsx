698ec38b9bcfcec02115a2a79674264451feddf2
Shaik Manisha <manishashaik0606@gmail.com>
2024-03-27 14:30:39 +0530

    Dynamic routing through Links

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,12 +1,23 @@
+import Link from "next/link";
 import React from "react";
 
 export default function ProductList() {
+  const productId = 100;
   return (
     <>
+      <Link href="/">Home</Link>
       <h1>Product List</h1>
-      <h1>Product 1</h1>
-      <h1>Product 2</h1>
-      <h1>Product 3</h1>
+      <h1>
+        <Link href="products/1">Product 1</Link>
+      </h1>
+      <h1>
+        <Link href="products/2">Product 2</Link>
+      </h1>
+      <h1>
+        <Link href={`products/${productId}`} replace>
+          Product {productId}
+        </Link>
+      </h1>
     </>
   );
 }
